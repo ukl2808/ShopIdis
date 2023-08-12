980fa0c0b0dcc8f41f0ea7f1bef91c0abfd9de2e
Admin <Admin@DESKTOP-047GBOQ>
2023-08-12 12:20:16 +0300

    Добавьте файлы проекта.

---
+++ ShopIdis/Program.cs
@@ -0,0 +1,9 @@
+﻿using (Shop shop = new Shop("Grocery Mart", "123 Main Street", ShopType.Grocery))
+{
+    Console.WriteLine("Shop Information:");
+    Console.WriteLine($"Name: {shop.Name}");
+    Console.WriteLine($"Address: {shop.Address}");
+    Console.WriteLine($"Type: {shop.Type}");
+}
+
+Console.WriteLine("Program completed.");
---
+++ ShopIdis/Shop.cs
@@ -0,0 +1,44 @@
+﻿enum ShopType
+{
+    Grocery,
+    Household,
+    Clothing,
+    Shoes
+}
+
+partial class Shop : IDisposable
+{
+    private string name;
+    private string address;
+    private ShopType type;
+
+    public Shop(string name, string address, ShopType type)
+    {
+        this.name = name;
+        this.address = address;
+        this.type = type;
+    }
+
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+
+    public string Address
+    {
+        get { return address; }
+        set { address = value; }
+    }
+
+    public ShopType Type
+    {
+        get { return type; }
+        set { type = value; }
+    }
+
+    public void Dispose()
+    {
+        Console.WriteLine($"Resource cleanup: Shop \"{Name}\".");
+    }
+}
